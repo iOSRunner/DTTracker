be58841007f172d70cf48a497ffa7add639143d8
@iOSRunner <@ericguo1213@163.com>
2017-05-17 20:29:53 +0800

    framework文件

---
+++ DTTracker/DTTrackManager.m
@@ -0,0 +1,17 @@
+//
+//  DTTrackManager.m
+//  DTTracker
+//
+//  Created by Wanlei on 2017/5/17.
+//  Copyright © 2017年 organizer. All rights reserved.
+//
+
+#import "DTTrackManager.h"
+
+@implementation DTTrackManager
+
+- (void)trackEvent:(NSString *)event {
+    NSLog(@"you are tracking event: %@", event);
+}
+
+@end
